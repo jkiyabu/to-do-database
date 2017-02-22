216f84ffab7f09130796a9dfc1bd42c731acb524
Jayeson Kiyabu and stella huayhuaca <stella@>
2017-02-21 16:36:44 -0800

    add category test

---
+++ tests/CategoryTest.php
@@ -0,0 +1,47 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Task.php";
+    require_once "src/Category.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CategoryTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Category::deleteAll();
+            Task::deleteAll();
+        }
+
+        function test_getTasks()
+        {
+            $name = "Home stuff";
+            $id = null;
+            $test_category = new Category($name, $id);
+            $test_category->save();
+
+            $test_category_id = $test_category->getId();
+
+            $description = "Wash clothes";
+            $test_task = new Task($description, $id, $test_category_id);
+            $test_task->save();
+
+            $description2 = "Do homework";
+            $test_task2 = new Task($description2, $id, $test_category_id);
+            $test_task2->save();
+
+            //Act
+            $result = $test_category->getTasks();
+
+            //Assert
+            $this->assertEquals([$test_task, $test_task2], $result);
+        }
+    }
+?>
